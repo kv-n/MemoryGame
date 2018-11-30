fc01aa1258389cfc0a6ee32d1258d30ce70980ca
Kevin <k.cuison@gmail.com>
2018-11-29 22:46:30 -0800

    Created Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,26 +27,34 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function () { 
-	if (cardsInPlay [0] === cardsInPlay [1]) {
-	console.log("You found a match!");
-	} else {
-	console.log("Sorry, try again.");
-	}
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert ("You found a match!")
+} else {
+	alert ("Sorry, try again.")
+}
 };
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push (cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log (cards[cardId].cardImage);
+	console.log (cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
-	if (cardsInPlay.length === 2);
-	else if (cardsInPlay [0] === cardsInPlay [1]) {
-	alert("You found a match!");
-	} else {
-	alert("Sorry, try again.");
 	}
 };
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src',"images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click',flipCard)
+	document.getElementById("game-board").appendChild(cardElement);
+}
+}
+
+createBoard();
